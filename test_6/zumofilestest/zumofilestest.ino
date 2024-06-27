90f77ae9e5dca2c1f7ad5c6a1fdc0d2b159491a0
Vissieboi <vishay2004@hotmail.com>
2024-06-27 13:49:40 +0200

    test 5

--- test_6/zumofilestest/zumofilestest.ino
+++ test_6/zumofilestest/zumofilestest.ino
@@ -1,31 +1,33 @@
-//test 5
+//test 6
 #include <Zumo32U4.h>
 #include "prox.h"
 #include <Wire.h>
 frontProxSensor proxSensors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4ButtonB buttonB;
+Zumo32U4ButtonC buttonC;
 Zumo32U4Motors motors;
 
 void setup() {
-  // put your setup code here, to run once:
-  // lineSensors.setup();
   buttonA.waitForButton();
-  // Serial.println("Knop A is ingedrukt");
-  // frontProxSensor.proxSetup();
 }
 
-bool loop_prox = false;
+bool status_ButtonB = false;
+bool status_ButtonC = false;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // lineSensors.loop();
   if (buttonB.isPressed()){//dit doe ik inplaats van dat het een bruine lijn leest, want dat lukte mij niet.
-      loop_prox = true;
+      status_ButtonB = true;
   }
-  else if(loop_prox == true){
+  else if(status_ButtonB == true){
     proxSensors.proxStart();//dit werkt
 
+  }
+  if (buttonC.isPressed()){
+    status_ButtonC = true;
+  }
+  while (status_ButtonC == true){
+    proxSensors.stop();
   }
       
 }
