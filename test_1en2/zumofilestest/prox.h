b647463ea8c74e2c66937e76a549b85c676e8181
Vissieboi <vishay2004@hotmail.com>
2024-06-27 12:33:48 +0200

    test 1 en 2

---
+++ test_1en2/zumofilestest/prox.h
@@ -0,0 +1,37 @@
+#ifndef PROX_H
+#define PROX_H
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+class frontProxSensor : public Zumo32U4ProximitySensors{
+
+    public:
+    frontProxSensor();
+    void proxSetup();
+    void proxLoop();
+    
+    private:
+    Zumo32U4Motors motors;
+    Zumo32U4ProximitySensors proxSensors;
+    Zumo32U4ButtonA buttonA;
+    Zumo32U4ButtonB buttonB;
+    
+    const uint8_t sensorThreshold = 1;
+    const uint16_t turnSpeedMax = 400;
+    const uint16_t turnSpeedMin = 100;
+    const uint16_t deceleration = 10;
+    const uint16_t acceleration = 10;
+
+    bool senseDir;
+    bool turningLeft;
+    bool turningRight;
+    uint16_t turnSpeed;
+    uint16_t lastTimeObjectSeen;
+
+    void stop();
+    void turnRight();
+    void turnLeft();
+    
+};
+
+#endif
